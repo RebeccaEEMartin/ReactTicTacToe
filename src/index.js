0719fbaeb6d51e02e736825210ef3373e9e1b388
Rebecca Martin <rebyk511@msn.com>
2018-08-06 19:45:54 +0100

    completed optional 3rd task

--- src/index.js
+++ src/index.js
@@ -22,26 +22,23 @@ class Board extends React.Component {
   }
 
   render() {
+    let squares = [];
+    for (let i=0; i<3; i++){
+      let row = [];
+      for (let j=0; j<3; j++){
+        row.push(this.renderSquare((i*3)+j));
+      }
+      squares.push(<div key={i} className="board-row">{ row }</div>);
+    }
+    console.log(squares);
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {squares}
       </div>
     );
   }
+
 }
 
 class Game extends React.Component {
